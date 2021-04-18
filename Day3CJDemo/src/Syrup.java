e33cb906668b685a86abece1ff070e30754532a2
Manthan Anjaria <manthan.anjaria@gmail.com>
2021-04-18 22:55:37 +0530

    Delete Syrup.java

--- Day3CJDemo/src/Syrup.java
+++
@@ -1,8 +0,0 @@
-
-public class Syrup extends Medicine {
-	@Override
-	public void displayLabel() {
-		// TODO Auto-generated method stub
-		System.out.println("Store in a Cool and Dry Place");
-	}
-}
